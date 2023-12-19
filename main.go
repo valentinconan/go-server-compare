330d00729e9244ae32d991e52658d46e8f56af57
valentinconan <valentin.conan@me.com>
2023-12-19 22:48:30 +0100

    Redefine echo using Group

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	//      10s test    port            -c 50                   -c 200                  -c 1000
 	go localGoGin()   //8080       305409 |  5,86MB/s      301142 |  5,76MB/s      305546 | 5,83MB/s
 	go localHttpMux() //8081       539316 | 10,34MB/s      584001 | 11,19MB/s      470207 | 9,00MB/s
-	go localEcho()    //8082       557608 | 10,69MB/s      509854 |  9,77MB/s      448974 | 8,59MB/s
+	go localEcho()    //8082       557608 | 10,69MB/s      509854 |  9,77MB/s      463519 | 8,89MB/s
 	go localMartini() //8083       251361 |  4,82MB/s      286005 |  5,48MB/s      271453 | 5,20MB/s
 	go localGoji()    //8084       552464 | 10,59MB/s      584054 | 11,19MB/s      450340 | 8,62MB/s
 	go localGorilla() //8085       554316 | 10,63MB/s      572607 | 10,98MB/s      446838 | 8,56MB/s
