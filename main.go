27d665c281f98b0079c94c7b2f9ce7ea6e8f2795
valentinconan <valentin.conan@me.com>
2023-12-21 21:18:06 +0100

    Extract gorilla server and sanitize it

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/go-chi/chi/v2"
 	"github.com/go-martini/martini"
 	"github.com/gofiber/fiber/v2"
-	gorilla "github.com/gorilla/mux"
 	"go-server-compare/servers"
 	"goji.io"
 	"goji.io/pat"
@@ -16,15 +15,15 @@ import (
 
 func main() {
 	slog.Info("Initializing router")
-	//         10s test          port            -c 50                   -c 200                  -c 1000
-	go localGoGin()             //8080       305409 |  5,86MB/s      301142 |  5,76MB/s      305546 | 5,83MB/s
-	go localHttpMux()           //8081       539316 | 10,34MB/s      584001 | 11,19MB/s      470207 | 9,00MB/s
-	go servers.NewEcho().Init() //8082       557608 | 10,69MB/s      509854 |  9,77MB/s      463519 | 8,89MB/s
-	go localMartini()           //8083       251361 |  4,82MB/s      286005 |  5,48MB/s      271453 | 5,20MB/s
-	go localGoji()              //8084       552464 | 10,59MB/s      584054 | 11,19MB/s      450340 | 8,62MB/s
-	go localGorilla()           //8085       554316 | 10,63MB/s      572607 | 10,98MB/s      446838 | 8,56MB/s
-	go localFiber()             //8086       608215 | 11,66MB/s      555507 | 10,65MB/s      498507 | 9,55MB/s
-	go localChi()               //8087       457159 |  8,76MB/s      439088 |  8,41MB/s      373024 | 7,14MB/s
+	//         10s test             port            -c 50                   -c 200                  -c 1000
+	go localGoGin()                //8080       305409 |  5,86MB/s      301142 |  5,76MB/s      305546 | 5,83MB/s
+	go localHttpMux()              //8081       539316 | 10,34MB/s      584001 | 11,19MB/s      470207 | 9,00MB/s
+	go servers.NewEcho().Init()    //8082       557608 | 10,69MB/s      509854 |  9,77MB/s      463519 | 8,89MB/s
+	go localMartini()              //8083       251361 |  4,82MB/s      286005 |  5,48MB/s      271453 | 5,20MB/s
+	go localGoji()                 //8084       552464 | 10,59MB/s      584054 | 11,19MB/s      450340 | 8,62MB/s
+	go servers.NewGorilla().Init() //8085       554316 | 10,63MB/s      572607 | 10,98MB/s      446838 | 8,56MB/s
+	go localFiber()                //8086       608215 | 11,66MB/s      555507 | 10,65MB/s      498507 | 9,55MB/s
+	go localChi()                  //8087       457159 |  8,76MB/s      439088 |  8,41MB/s      373024 | 7,14MB/s
 	var forever chan struct{}
 	slog.Info("Infinite loop")
 	<-forever
@@ -61,15 +60,6 @@ func localMartini() {
 	m.RunOnAddr("localhost:8083")
 }
 
-func localGorilla() {
-	slog.Info("Initializing gorilla")
-	router := gorilla.NewRouter()
-	router.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "{\"status\": \"OK\"}")
-	})
-
-	http.ListenAndServe("localhost:8085", router)
-}
 func localHttpMux() {
 	slog.Info("Initializing mux")
 	mux := http.NewServeMux()
--- servers/gorilla.go
+++ servers/gorilla.go
@@ -1,4 +1,4 @@
-package main
+package servers
 
 import (
 	"fmt"
@@ -8,16 +8,14 @@ import (
 	"net/http"
 )
 
-func main() {
-	slog.Info("Initializing router")
-	//      10s test    port            -c 50                   -c 200                  -c 1000
-	go localGorilla() //8085       554316 | 10,63MB/s      572607 | 10,98MB/s      446838 | 8,56MB/s
-	var forever chan struct{}
-	slog.Info("Infinite loop")
-	<-forever
+type Gorilla struct {
 }
 
-func localGorilla() {
+func NewGorilla() *Gorilla {
+	return &Gorilla{}
+}
+
+func (g *Gorilla) Init() {
 	slog.Info("Initializing gorilla")
 
 	router := mux.NewRouter()
@@ -38,5 +36,5 @@ func localGorilla() {
 		fmt.Fprint(w, "{\"test\": \"KO\"}")
 	})
 
-	log.Fatal(http.ListenAndServe("localhost:8081", router))
+	log.Fatal(http.ListenAndServe("localhost:8085", router))
 }
