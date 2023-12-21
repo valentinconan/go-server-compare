7db7ed169c737d54628da35518e224063a18ecaa
valentinconan <valentin.conan@me.com>
2023-12-21 21:07:31 +0100

    Extract echo server and sanitize it

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/go-martini/martini"
 	"github.com/gofiber/fiber/v2"
 	gorilla "github.com/gorilla/mux"
-	"github.com/labstack/echo"
+	"go-server-compare/servers"
 	"goji.io"
 	"goji.io/pat"
 	"log/slog"
@@ -16,15 +16,15 @@ import (
 
 func main() {
 	slog.Info("Initializing router")
-	//      10s test    port            -c 50                   -c 200                  -c 1000
-	go localGoGin()   //8080       305409 |  5,86MB/s      301142 |  5,76MB/s      305546 | 5,83MB/s
-	go localHttpMux() //8081       539316 | 10,34MB/s      584001 | 11,19MB/s      470207 | 9,00MB/s
-	go localEcho()    //8082       557608 | 10,69MB/s      509854 |  9,77MB/s      463519 | 8,89MB/s
-	go localMartini() //8083       251361 |  4,82MB/s      286005 |  5,48MB/s      271453 | 5,20MB/s
-	go localGoji()    //8084       552464 | 10,59MB/s      584054 | 11,19MB/s      450340 | 8,62MB/s
-	go localGorilla() //8085       554316 | 10,63MB/s      572607 | 10,98MB/s      446838 | 8,56MB/s
-	go localFiber()   //8086       608215 | 11,66MB/s      555507 | 10,65MB/s      498507 | 9,55MB/s
-	go localChi()     //8087       457159 |  8,76MB/s      439088 |  8,41MB/s      373024 | 7,14MB/s
+	//         10s test          port            -c 50                   -c 200                  -c 1000
+	go localGoGin()             //8080       305409 |  5,86MB/s      301142 |  5,76MB/s      305546 | 5,83MB/s
+	go localHttpMux()           //8081       539316 | 10,34MB/s      584001 | 11,19MB/s      470207 | 9,00MB/s
+	go servers.NewEcho().Init() //8082       557608 | 10,69MB/s      509854 |  9,77MB/s      463519 | 8,89MB/s
+	go localMartini()           //8083       251361 |  4,82MB/s      286005 |  5,48MB/s      271453 | 5,20MB/s
+	go localGoji()              //8084       552464 | 10,59MB/s      584054 | 11,19MB/s      450340 | 8,62MB/s
+	go localGorilla()           //8085       554316 | 10,63MB/s      572607 | 10,98MB/s      446838 | 8,56MB/s
+	go localFiber()             //8086       608215 | 11,66MB/s      555507 | 10,65MB/s      498507 | 9,55MB/s
+	go localChi()               //8087       457159 |  8,76MB/s      439088 |  8,41MB/s      373024 | 7,14MB/s
 	var forever chan struct{}
 	slog.Info("Infinite loop")
 	<-forever
@@ -91,14 +91,4 @@ func localGoGin() {
 	if err := router.Run(":8080"); err != nil {
 		slog.Error("Erreur lors du lancement du serveur Gin: %v", err)
 	}
-}
-
-func localEcho() {
-	slog.Info("Initializing echo")
-	e := echo.New()
-	//QueryParam along with PathVariable Get API
-	e.GET("/health*", func(c echo.Context) error {
-		return c.String(http.StatusOK, fmt.Sprintf("{\"status\": \"OK\"}"))
-	})
-	e.Start("localhost:8082")
 }
