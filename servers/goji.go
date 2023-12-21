dae1a95d0b982d317c375bdefe5f2053dffa5197
valentinconan <valentin.conan@me.com>
2023-12-21 22:19:03 +0100

    Extract goji server and sanitize it

---
+++ servers/goji.go
@@ -0,0 +1,39 @@
+package servers
+
+import (
+	"fmt"
+	goji "goji.io"
+	"goji.io/pat"
+	"net/http"
+)
+
+type Goji struct {
+}
+
+func NewGoji() *Goji {
+	return &Goji{}
+}
+
+func (g *Goji) Init() {
+	mux := goji.NewMux()
+
+	mux.Use(func(handler http.Handler) http.Handler {
+		//do whatever you need here
+		return handler
+	})
+
+	mux.HandleFunc(pat.Get("/health"), func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "{\"status\": \"OK\"}")
+	})
+
+	testGroup := goji.SubMux()
+	mux.Handle(pat.New("/test/*"), testGroup)
+	testGroup.HandleFunc(pat.Get("/ok"), func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "{\"test\": \"OK\"}")
+	})
+	testGroup.HandleFunc(pat.Get("/ko"), func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "{\"test\": \"KO\"}")
+	})
+
+	http.ListenAndServe("localhost:8084", mux)
+}
