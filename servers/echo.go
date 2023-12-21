00e19b702bd76ecf3c4be01e34cb7d8d67c914a7
valentinconan <valentin.conan@me.com>
2023-12-21 21:07:31 +0100

    Extract echo server and sanitize it

--- servers/echo.go
+++ servers/echo.go
@@ -1,4 +1,4 @@
-package main
+package servers
 
 import (
 	"fmt"
@@ -7,32 +7,31 @@ import (
 	"net/http"
 )
 
-func main() {
-	slog.Info("Initializing router")
-	//      10s test    port            -c 50                   -c 200                  -c 1000
-	go localEcho() //8082       557608 | 10,69MB/s      509854 |  9,77MB/s      463519 | 8,89MB/s
-	var forever chan struct{}
-	slog.Info("Infinite loop")
-	<-forever
+type Echo struct {
 }
-func localEcho() {
-	slog.Info("Initializing echo")
-	e := echo.New()
 
-	e.Use(func(handlerFunc echo.HandlerFunc) echo.HandlerFunc {
+func NewEcho() *Echo {
+	return &Echo{}
+}
+
+func (e *Echo) Init() {
+	slog.Info("Initializing echo")
+	server := echo.New()
+
+	server.Use(func(handlerFunc echo.HandlerFunc) echo.HandlerFunc {
 		//do whatever you need here
 		return handlerFunc
 	})
 	//QueryParam along with PathVariable Get API
-	e.GET("/health*", func(c echo.Context) error {
+	server.GET("/health*", func(c echo.Context) error {
 		return c.String(http.StatusOK, fmt.Sprintf("{\"status\": \"OK\"}"))
 	})
-	testGroup := e.Group("test")
+	testGroup := server.Group("test")
 	testGroup.GET("/ok", func(c echo.Context) error {
 		return c.String(http.StatusOK, fmt.Sprintf("{\"test\": \"OK\"}"))
 	})
 	testGroup.GET("/ko", func(c echo.Context) error {
 		return c.String(http.StatusOK, fmt.Sprintf("{\"test\": \"KO\"}"))
 	})
-	e.Start("localhost:8082")
+	server.Start("localhost:8082")
 }
